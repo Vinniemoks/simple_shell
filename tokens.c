d4dbc7888bade11e2e8c856e32f171c194573248
Vincent Mokua <mokua.vinny@gmail.com>
2024-08-29 00:31:58 -0700

    tf!

--- tokens.c
+++ tokens.c
@@ -1,18 +1,41 @@
-#include <stdio.h>
-#include <string.h>
+#include "shell.h"
+/**
+ * tokenize - this function separate the string using a designed delimiter
+ * @data: a pointer to the program's data
+ * Return: an array of the different parts of the string
+ */
+void tokens(data_of_program *data)
+{
+	char *delimiter = " \t";
+	int i, j, counter = 2, length;
 
-int main() {
-    char command[] ="Your,name,is,mokua";
-    char *token;
-    char *delim = ",";
-   
+	length = str_length(data->input_line);
+	if (length)
+	{
+		if (data->input_line[length - 1] == '\n')
+			data->input_line[length - 1] = '\0';
+	}
 
-    token = strtok(command, delim);
-   
-    while (token != NULL) {
-        printf("Token: %s\n", token);
-        token = strtok(NULL, delim); 
-    }
+	for (i = 0; data->input_line[i]; i++)
+	{
+		for (j = 0; delimiter[j]; j++)
+		{
+			if (data->input_line[i] == delimiter[j])
+				counter++;
+		}
+	}
 
-    return 0;
+	data->tokens = malloc(counter * sizeof(char *));
+	if (data->tokens == NULL)
+	{
+		perror(data->program_name);
+		exit(errno);
+	}
+	i = 0;
+	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
+	data->command_name = str_duplicate(data->tokens[0]);
+	while (data->tokens[i++])
+	{
+		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+	}
 }
